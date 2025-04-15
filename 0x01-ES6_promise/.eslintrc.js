022662feac9ae71c8ab868e99e6805489863eabd
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-15 09:37:10 +0200

    Update .eslintrc.js

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -2,13 +2,34 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    node: true,
     jest: true,
   },
-  extends: ['airbnb-base'],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
   rules: {
     'no-console': 'off',
-    'no-unused-vars': 'off',
-    'no-underscore-dangle': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
