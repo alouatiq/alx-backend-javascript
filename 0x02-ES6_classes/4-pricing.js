ea566a77f206b24251ada41b3f436eb5b34cd301
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-17 22:30:00 +0200

    Update 4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -6,11 +6,21 @@ export default class Pricing {
     this._currency = currency;
   }
 
-  get amount() { return this._amount; }
-  set amount(val) { this._amount = val; }
+  get amount() {
+    return this._amount;
+  }
 
-  get currency() { return this._currency; }
-  set currency(val) { this._currency = val; }
+  set amount(value) {
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
 
   displayFullPrice() {
     return `${this._amount} ${this._currency.name} (${this._currency.code})`;
