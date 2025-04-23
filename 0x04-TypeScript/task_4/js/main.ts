c3dabdeee6cd5b1e93764185d826e5ff7a6fb358
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-04-23 10:52:29 +0200

    Create main.ts

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,33 @@
+// task_4/js/main.ts
+
+import { Subjects } from './subjects/Cpp';
+import './subjects/Java';
+import './subjects/React';
+
+export const cpp = new Subjects.Cpp();
+export const java = new Subjects.Java();
+export const react = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  experienceTeachingC: 10
+};
+
+// C++
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
