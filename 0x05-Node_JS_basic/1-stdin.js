b691f8e753ff7a11819097e87880b4853a94700c
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-06-11 09:24:00 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,13 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\r`);
-  process.stdin.end();
+  console.log(`Your name is: ${name}`);
 });
 
+// Handle EOF or end of stdin
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
 });
