03daaa469cbb5ad2f6656ed2b46325dc8dbc7cea
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-06-11 09:00:20 +0200

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,11 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 
-rl.on('close', () => {
+process.on('exit', () => {
   console.log('This important software is now closing');
 });
