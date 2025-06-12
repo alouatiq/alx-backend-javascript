5388132e84e57a941f50d52e9d0e607dc72e24f7
AL OUATIQ H. <24354854+alouatiq@users.noreply.github.com>
2025-06-12 21:07:34 +0200

    Create 5-payment.test.js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi with hooks', function () {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with 100 and 20', function () {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('should log "The total is: 20" when called with 10 and 10', function () {
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+  });
+});
